6592cc186f6502633e2bbc6be340827762397377
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-04-24 16:24:59 -0300

    js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-function App() {
-  return (
-    <h1>
-      Olá, Mundo!
-    </h1>
-      
+import SobreMim from "./paginas/SobreMim";
+import Inicio from "./paginas/inicio";
+
+console.log(window.location);
+
+const pagina =
+  window.location.pathname === "/" ? (
+    <Inicio/>
+  ) : (
+    <SobreMim/>
   );
+
+function App() {
+  return pagina;
 }
 
 export default App;
