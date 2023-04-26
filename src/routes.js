35464e1c5424b2967e7ffb4a834936a4952fe7e1
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-04-26 16:29:09 -0300

    rotas aninhadas

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import SobreMim from "./paginas/SobreMim";
 import Inicio from "./paginas/Inicio";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function AppRoutes() {
@@ -9,8 +10,12 @@ function AppRoutes() {
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          {/* <Route path="/" element={<Inicio />} /> */}
+          <Route index element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+        </Route>
+
         <Route path="*" element={<div>Página não encontrada...</div>} />
       </Routes>
       <Rodape />
