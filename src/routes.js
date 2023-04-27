e7597b36b35e0e5acc137351680c939e72069d8c
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-04-27 16:06:56 -0300

    scroll to top

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,12 @@ import NaoEncontrada from "paginas/NaoEncontrada";
 import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "componentes/ScrollToTop";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Menu />
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
@@ -18,7 +20,7 @@ function AppRoutes() {
           <Route path="sobremim" element={<SobreMim />} />
         </Route>
 
-        <Route path="posts/:id" element={<Post />} />
+        <Route path="posts/:id/*" element={<Post />} />
         <Route path="*" element={<NaoEncontrada />} />
       </Routes>
       <Rodape />
